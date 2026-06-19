123d86b9694749c964f34e3fdd29709bf8edaf2e
CruzAlanna <lannikcru@gmail.com>
2026-06-19 10:34:34 +1000

    fix cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://indecisiveapp.netlify.app/'
+    origins 'https://indecisiveapp.netlify.app'
     resource '*',
     headers: :any,
     methods: [:get, :post, :put, :patch, :delete, :options, :head]
